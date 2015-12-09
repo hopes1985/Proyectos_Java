6a2869f81b59e833a51638a89dfcafaa9472123b
Javi <hopes1985@gmail.com>
2015-12-09 12:17:36 +0100

    version 2

---
+++ src/Principal21.java
@@ -0,0 +1,8 @@
+
+public class Principal21 {
+	public static int producto(int a, int b) {
+		int c;
+		c = a * b;
+		return c;
+		}
+}
---
+++ src/Principal22.java
@@ -0,0 +1,8 @@
+
+public class Principal22 {
+	public static int dividir(int a, int b) {
+		int c;
+		c = a / b;
+		return c;
+		}
+}
